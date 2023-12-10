dccf0ffc6056addee03ff753e91063fb63af81df
nqianhuiii <nghui@graduate.utm.my>
2023-12-10 09:48:06 +0800

    done revert

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'IKON Chat',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.grey[200]),
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(
+            color: Colors.black, fontWeight: FontWeight.normal, fontSize: 19),
+        backgroundColor: Colors.white,
+      )),
       home: const BottomNavBar(),
     );
   }
