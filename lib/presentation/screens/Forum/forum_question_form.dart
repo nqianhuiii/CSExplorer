3e6f0c29e266dbf77f151996c72f943f0248ce4b
wenzhen1213 <150309706+wenzhen1213@users.noreply.github.com>
2023-12-30 02:48:54 +0800

    Update forum_question_form.dart

--- lib/presentation/screens/Forum/forum_question_form.dart
+++ lib/presentation/screens/Forum/forum_question_form.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:csexplorer/data/model/forum.dart';
 import 'package:csexplorer/data/repositories/forum_repo.dart';
+import 'package:csexplorer/service/authService.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ForumTopicForm extends StatefulWidget {
   const ForumTopicForm({super.key});
@@ -14,6 +18,39 @@ class _ForumTopicFormState extends State<ForumTopicForm> {
   final _messageController = TextEditingController();
   final ForumRepository _forumRepository = ForumRepository();
   int likes = 0;
+  String userId = AuthService.getCurrentUserId();
+  String name = "";
+  XFile? _image;
+  String imagePathAsString = "";
+
+  Future<void> _getImage() async {
+    // ignore: no_leading_underscores_for_local_identifiers
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      setState(() {
+        _image = image;
+        imagePathAsString = _image?.path ?? '';
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeName();
+  }
+
+  Future<void> initializeName() async {
+    // ignore: await_only_futures
+    print("id,$userId");
+    name = await AuthService.getUserNameById(userId);
+    print("name,$name");
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,15 +85,34 @@ class _ForumTopicFormState extends State<ForumTopicForm> {
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10)))),
-              const SizedBox(height: 271),
+              const SizedBox(height: 15),
+              const Text("Upload Image"),
+              const SizedBox(
+                height: 5,
+              ),
+              _image != null
+                  ? Image.file(
+                      File(_image!.path),
+                      height: 100,
+                    )
+                  : ElevatedButton(
+                      onPressed: _getImage,
+                      child: const Text("Select Image"),
+                    ),
+              const SizedBox(height: 156),
               Center(
                 child: SizedBox(
                   width: 250,
                   height: 50,
                   child: ElevatedButton(
                     onPressed: () async {
-                      Forum forum = Forum(_subjectController.text,
-                          _messageController.text, likes);
+                      Forum forum = Forum(
+                          _subjectController.text,
+                          _messageController.text,
+                          likes,
+                          userId,
+                          name,
+                          imagePathAsString);
                       _forumRepository.addForumTopic(forum);
                       Navigator.pop(context);
                     },
@@ -76,4 +132,5 @@ class _ForumTopicFormState extends State<ForumTopicForm> {
     );
   }
 }
+
 
