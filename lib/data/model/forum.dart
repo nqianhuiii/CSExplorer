b3fc152aa0663000333f5eeb59fefd435b9c9b81
wenzhen1213 <150309706+wenzhen1213@users.noreply.github.com>
2023-12-30 02:47:07 +0800

    Update forum.dart

--- lib/data/model/forum.dart
+++ lib/data/model/forum.dart
@@ -1,18 +1,34 @@
-class Forum {
+class Forum
+{
   late String subject, message;
-  late int likes=0;
+  late int likes = 0;
+  late String id;
+  late String name;
+  late String image;
 
-  Forum(this.subject, this.message, this.likes);
+  Forum(this.subject, this.message, this.likes, this.id, this.name, this.image);
 
-  Forum.copy(Forum item) : this(item.subject, item.message, item.likes);
+  Forum.copy(Forum item)
+      : this(item.subject, item.message, item.likes, item.id, item.name,
+            item.image);
 
   Map<String, dynamic> toJson() {
-    return {'subject': subject, 'message': message, 'likes': likes};
+    return {
+      'subject': subject,
+      'message': message,
+      'likes': likes,
+      'id': id,
+      'name': name,
+      'image': image,
+    };
   }
 
   Forum.fromJson(Map<String, dynamic> json) {
     subject = json['subject'];
     message = json['message'];
     likes = json['likes'];
+    id = json['id'];
+    name = json['name'];
+    image = json['image'];
   }
 }
